26245902e74c1211c483796c0c10c8f842781f15
nascimentoJoao <joaozinhusama@gmail.com>
2020-07-15 01:07:48 -0300

    Remove Bean annotation import.

--- src/main/java/com/example/demo/client/PoaTransporteItinerarioRestClient.java
+++ src/main/java/com/example/demo/client/PoaTransporteItinerarioRestClient.java
@@ -1,7 +1,6 @@
 package com.example.demo.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
